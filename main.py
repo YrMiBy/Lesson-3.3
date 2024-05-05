3349a4a3b081c21f362d1627a6550f3dca5c2a6c
YrMiBy <ymbykov@gmail.com>
2024-05-05 15:18:14 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,7 +7,15 @@ SCREEN_WIDTH = 800 # переменная - ширина экрана
 SCREEN_HEIGHT = 600 # переменная высота экрана
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # переменная-создание экрана (двойные скобки - кортеж)
 pygame.display.set_caption('Игра Тир') # заголовок окна
-icon = pygame.image.load() # переменная для иконки
+icon = pygame.image.load('img/icons.png') # переменная для иконки в созданной директории img
+pygame.display.set_icon(icon) # установка иконки
+
+target_image = pygame.image.load('img/target.png') # переменная определяет мишень 
+target_width = 80 # ширина мишени
+target_height = 80 # высота мишени
+target_x = random.randint(0, SCREEN_WIDTH - target_width)# перемещение мишени по ширине экрана
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)# перемещение мишени по высоте экрана
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))# изменеие цвета экрана
 
 running = True
 while running:
