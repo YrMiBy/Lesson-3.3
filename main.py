e8dda551aacef60da40bc6eadb33529f9952e212
YrMiBy <ymbykov@gmail.com>
2024-05-05 13:43:50 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800 # переменная - ширина экрана
+SCREEN_HEIGHT = 600 # переменная высота экрана
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # переменная-создание экрана (двойные скобки - кортеж)
+pygame.display.set_caption('Игра Тир') # заголовок окна
+icon = pygame.image.load() # переменная для иконки
+
 running = True
 while running:
     pass
